0c10f7ec5d0cf33a078cd5232469c77916360e85
Blair Davidson <blaird@signiq.com>
2016-11-04 15:04:13 +0800

    FromURI stuff

---
+++ ScratchAPI/Controllers/CoordinatesController.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.Http;
+using System.Web.Http;
+
+namespace ScratchAPI.Controllers
+{
+
+    public class Coordinate
+    {
+        public double Lattitude { get; set; }
+        public double Longitude { get; set; }
+    }
+
+    public class CoordinatesController : ApiController
+    {
+        [HttpGet]
+        public HttpResponseMessage Post([FromUri] Coordinate coordinates) => Request.CreateResponse(coordinates);
+    }
+}
